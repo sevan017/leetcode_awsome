f4dfa3abfc51e8e1091c7b17a81e93f7c17fcb1e
17 <2190638312@qq.com>
2026-07-17 07:06:22 +0800

    feat(daily): 3312 查询排序后的最大公约数 | 2026-07-17

---
+++ src/main/java/daily_question/_2026/_0717/README.md
@@ -0,0 +1,5 @@
+# 3312. 查询排序后的最大公约数
+- 日期：2026-07-17
+- 原题链接：[https://leetcode.cn/problems/sorted-gcd-pair-queries/](https://leetcode.cn/problems/sorted-gcd-pair-queries/)
+- 题目标识：sorted-gcd-pair-queries
+- 是否付费：否
---
+++ src/main/java/daily_question/_2026/_0717/sorted_gcd_pair_queries.java
@@ -0,0 +1,21 @@
+package daily_question._2026._0717;
+/**
+ * LeetCode 每日一题
+ * 题号：3312
+ * 题目：查询排序后的最大公约数
+ * 链接：https://leetcode.cn/problems/sorted-gcd-pair-queries/
+ * 日期：2026-07-17
+ */
+public class sorted_gcd_pair_queries {
+    public static void main(String[] args) {
+        // 测试入口
+    }
+
+    static class Solution {
+        // 自定义解题方法，根据题目需求补充实现
+        public long gcdSum(int[] nums) {
+
+            return 0L;
+        }
+    }
+}
