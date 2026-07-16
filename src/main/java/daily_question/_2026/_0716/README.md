faa4afa6da3d4aac8cf39a97a89d530699907305
17 <2190638312@qq.com>
2026-07-16 11:45:10 +0800

    feat(daily): 3867 数对的最大公约数之和 | 2026-07-16

---
+++ src/main/java/daily_question/_2026/_0716/README.md
@@ -0,0 +1,5 @@
+# 3867. 数对的最大公约数之和
+- 日期：2026-07-16
+- 原题链接：[https://leetcode.cn/problems/sum-of-gcd-of-formed-pairs/](https://leetcode.cn/problems/sum-of-gcd-of-formed-pairs/)
+- 题目标识：sum-of-gcd-of-formed-pairs
+- 是否付费：否
---
+++ src/main/java/daily_question/_2026/_0716/sum_of_gcd_of_formed_pairs.java
@@ -0,0 +1,21 @@
+package daily_question._2026._0716;
+/**
+ * LeetCode 每日一题
+ * 题号：3867
+ * 题目：数对的最大公约数之和
+ * 链接：https://leetcode.cn/problems/sum-of-gcd-of-formed-pairs/
+ * 日期：2026-07-16
+ */
+public class sum_of_gcd_of_formed_pairs {
+    public static void main(String[] args) {
+        // 测试入口
+    }
+
+    static class Solution {
+        // 自定义解题方法，根据题目需求补充实现
+        public long gcdSum(int[] nums) {
+
+            return 0L;
+        }
+    }
+}
