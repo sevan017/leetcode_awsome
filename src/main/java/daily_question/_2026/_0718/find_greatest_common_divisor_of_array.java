2fe49986f2b539e89a4d8807522ecc20c3be0eaa
17 <2190638312@qq.com>
2026-07-18 07:13:10 +0800

    feat(daily): 1979. 找出数组的最大公约数 | 2026-07-18

---
+++ src/main/java/daily_question/_2026/_0718/find_greatest_common_divisor_of_array.java
@@ -0,0 +1,28 @@
+package daily_question._2026._0718;
+
+public class find_greatest_common_divisor_of_array {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.findGCD(new int[]{7, 5, 6, 8, 3});
+    }
+
+    static class Solution {
+        public int findGCD(int[] nums) {
+            int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+            for (int i = 0; i < nums.length; i++) {
+                min = Math.min(min, nums[i]);
+                max = Math.max(max, nums[i]);
+            }
+            System.out.println(max + " " + min);
+
+            while (min != 0) {
+//                顺序不能换，如果max 先被min覆盖，那min接下来怎么取都只会是0
+                int tmp = min;
+                min = max % min;
+                max = tmp;
+            }
+            return max;
+        }
+    }
+}
