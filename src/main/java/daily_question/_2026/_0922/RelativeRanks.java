a45d19571687cb68a6b37611409bd8082db321de
17 <2190638312@qq.com>
2026-09-22 16:20:02 +0800

    直接降序排序即可

--- src/main/java/daily_question/_2026/_0922/RelativeRanks.java
+++ src/main/java/daily_question/_2026/_0922/RelativeRanks.java
@@ -5,7 +5,34 @@ import java.util.Arrays;
 // https://leetcode.cn/problems/relative-ranks/?envType=problem-list-v2&envId=sorting&
 public class RelativeRanks {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(findRelativeRanks(new int[]{5, 4, 3, 2, 1})));
+//        System.out.println(Arrays.toString(findRelativeRanks(new int[]{5, 4, 3, 2, 1})));
+        System.out.println(Arrays.toString(findRelativeRanks2(new int[]{5, 4, 3, 2, 1})));
+    }
+
+    public static String[] findRelativeRanks2(int[] score) {
+        int n = score.length;
+        int[][] s_index = new int[n][2];
+        // 记录每个分数所在的下标
+        for (int i = 0; i < n; i++) {
+            s_index[i][0] = score[i];
+            s_index[i][1] = i;
+        }
+        String[] ans = new String[n];
+        // 按分数降序排序
+        Arrays.sort(s_index, (a, b) -> b[0] - a[0]);
+        for (int i = 0; i < n; i++) {
+            int index = s_index[i][1];
+            if (i == 0) {
+                ans[index] = "Gold Medal";
+            } else if (i == 1) {
+                ans[index] = "Silver Medal";
+            } else if (i == 2) {
+                ans[index] = "Bronze Medal";
+            } else {
+                ans[index] = String.valueOf(i + 1);
+            }
+        }
+        return ans;
     }
 
     public static String[] findRelativeRanks(int[] score) {
